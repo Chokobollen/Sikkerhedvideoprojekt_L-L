5420127407a5a8e6c0327b7060045fcf650535af
Chokobollen <unordleefif@gmail.com>
2022-05-01 14:03:31 +0200

    Created new scene plus added et fungerende script

--- Assets/Scripts/TextToggleTrigger.cs
+++ Assets/Scripts/TextToggleTrigger.cs
@@ -1,23 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class HeadCollider : MonoBehaviour
+public class TextToggleTrigger : MonoBehaviour
 {
+
     void Start()
     {
         //Set the tag of this GameObject to Player
-        gameObject.tag = "Player";
+        gameObject.tag = "Hand";
     }
 
+    public GameObject Textboks;
     private void OnTriggerEnter(Collider other)
     {
         //Check to see if the tag on the collider is equal to Enemy
         if (other.tag == "Brille")
         {
-            SceneManager.LoadScene(0);
+            Textboks.GetComponent<Renderer>().enabled = false;
         }
     }
-
 }
