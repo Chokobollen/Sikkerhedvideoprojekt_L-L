6e723c25448a31e253d5741fe14c1b91a46a241e
Chokobollen <unordleefif@gmail.com>
2022-05-01 18:43:44 +0200

    Fikset script errors

--- Assets/Scripts/Sikkerhedsbrille challange.cs
+++ Assets/Scripts/Sikkerhedsbrille challange.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class TextToggleTrigger : MonoBehaviour
+public class SikkerhedsbrilleChallange : MonoBehaviour
 {
 
     void Start()
--- Assets/Scripts/TextToggleTrigger.cs
+++ Assets/Scripts/TextToggleTrigger.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class TextToggleTrigger : MonoBehaviour
+
+public class TextToggleTrigger: MonoBehaviour
 {
+    public GameObject Textboks;
 
     void Start()
     {
@@ -10,7 +12,7 @@ public class TextToggleTrigger : MonoBehaviour
         gameObject.tag = "Hand";
     }
 
-    public GameObject Textboks;
+
     private void OnTriggerEnter(Collider other)
     {
         //Check to see if the tag on the collider is equal to Enemy
